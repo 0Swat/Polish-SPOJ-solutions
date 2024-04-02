80b5a8fa841ca35c8f2c6383793b5d96ee5c0f17
Oskar Swat <oskar.swat@tlen.pl>
2024-04-02 16:12:18 +0200

    https://pl.spoj.com/problems/SKARBFI/

--- SkarbFinder/main.py
+++ SkarbFinder/main.py
@@ -5,21 +5,21 @@ def Odpowiedz(x, y):
         if y > 0:
             print(3, y)
         else:
-            print(2, y)
+            print(2, y*-1)
     if x != 0 and y == 0:
         if x > 0:
             print(0, x)
         else:
-            print(1, x)
+            print(1, x*-1)
     if x != 0 and y != 0:
         if x > 0:
             print(0, x)
         else:
-            print(1, x)
+            print(1, x*-1)
         if y > 0:
             print(3, y)
         else:
-            print(2, y)
+            print(2, y*-1)
 
 d = int(input())
 while d:
