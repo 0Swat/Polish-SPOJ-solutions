042d3c3b5b067b3d8ae849cbecce6f7749aa1aec
Oskar Swat <oskar.swat@tlen.pl>
2024-05-25 13:04:39 +0200

    https://pl.spoj.com/problems/BEZPRZEC/

---
+++ Czy potrafisz nie uzyc srednika/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+	if(int a = 1)
+	{
+		if(int b = 1)
+		{
+			if(std::cin >> a)
+			{
+				if(std::cin >> b)
+				{
+					if(std::cout << a+b)
+					{
+		
+					}
+				}
+					
+			}	
+		}
+	}
+}
