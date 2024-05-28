e149907ef7922eca4747885cd0786e6bbc1e3e9d
Oskar Swat <oskar.swat@tlen.pl>
2024-05-28 18:53:22 +0200

    Timeout

--- Trójkątne Pola/main.py
+++ Trójkątne Pola/main.py
@@ -1,51 +1,65 @@
-def is_acute_triangle(p1, p2, p3):
-    def squared_distance(a, b):
-        return (a[0] - b[0])**2 + (a[1] - b[1])**2
-    
-    a2 = squared_distance(p2, p3)
-    b2 = squared_distance(p1, p3)
-    c2 = squared_distance(p1, p2)
-    
-    return (a2 + b2 > c2) and (a2 + c2 > b2) and (b2 + c2 > a2)
+import math
 
-def solve_triangle_fields(input_data):
-    import sys
-    input = sys.stdin.read
-    data = input_data.split()
-    
-    index = 0
-    D = int(data[index])
-    index += 1
-    results = []
-    
-    for _ in range(D):
-        N = int(data[index])
-        index += 1
-        points = []
-        for i in range(N):
-            x = int(data[index])
-            y = int(data[index + 1])
-            points.append((x, y, i + 1))  # Store with 1-based index
-            index += 2
-        
-        points.sort()
-        
-        triangles = []
-        for i in range(0, N, 3):
-            p1, p2, p3 = points[i], points[i+1], points[i+2]
-            if is_acute_triangle(p1, p2, p3):
-                triangles.append(sorted([p1[2], p2[2], p3[2]]))
-        
-        results.append(triangles)
-    
-    output = []
-    for result in results:
-        for triangle in result:
-            output.append(" ".join(map(str, triangle)))
-        output.append("")  # Separate different datasets by a blank line
-    
-    return "\n".join(output)
+class Field:
+    def __init__(self, n):
+        self.triangles = []
 
-# Example usage:
-input_data = "2\n3\n0 0\n1 2\n2 1\n6\n-2 0\n2 0\n1 1\n-1 1\n0 3\n0 4\n"
-print(solve_triangle_fields(input_data))
+        for i in range(1, n+1):
+            vertex = []
+            x, y = map(int, input().split())
+            vertex.append(i)
+            vertex.append(x)
+            vertex.append(y)
+            self.triangles.append(vertex)
+        
+        while self.triangles:
+            self.Graham()
+        
+
+    def Graham(self):
+        self.triangles.sort(key=lambda x: x[1])
+
+        triangle = []
+        triangle.append(self.triangles[0])
+        maks = [False, 0]
+        min = [False, 0]
+        for i in range(1, len(self.triangles)):
+            vector = (self.triangles[i][1] - self.triangles[0][1], self.triangles[i][2] - self.triangles[0][2])
+            angle = math.atan2(vector[1], vector[0])
+
+            if maks[0] == False:
+                maks[0] = self.triangles[1]
+                maks[1] = angle
+            else:
+                if maks[1] < angle:
+                    maks[0] = self.triangles[i]
+                    maks[1] = angle
+
+            if min[0] == False:
+                min[0] = self.triangles[1]
+                min[1] = angle
+            else:
+                if min[1] > angle:
+                    min[0] = self.triangles[i]
+                    min[1] = angle
+
+        triangle.append(min[0])
+        triangle.append(maks[0])
+        triangle.sort(key=lambda x: x[0])
+
+        print(str(triangle[0][0]) + " " + str(triangle[1][0]) + " " + str(triangle[2][0]))
+
+        self.triangles.pop(0)
+        self.triangles.remove(min[0])
+        self.triangles.remove(maks[0])
+
+
+def main():
+    d= int(input())
+    while d:
+        n = int(input())
+        Field(n)
+        d-=1
+
+if __name__ == '__main__':
+    main()
