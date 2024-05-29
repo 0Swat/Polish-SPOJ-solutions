5e50e7e94f7b5ad12dac95388ecd6a87622c5998
Oskar Swat <oskar.swat@tlen.pl>
2024-05-29 16:28:56 +0200

    Wrong answers - probably forgot to take into account that mounds can be in the same path

--- Jesienne porządki/main.py
+++ Jesienne porządki/main.py
@@ -1,5 +1,19 @@
-import math
+def answer(data):
+    kopiec = mediana(data)
+    dlugosc = 0
 
+    for i in range(len(data)):
+        dlugosc += length(data[i], kopiec)
+        print(dlugosc)
+
+    print(str(kopiec) + " " + str(dlugosc))
+
+
+def length(punkt, x):
+    dlugosc_x = abs(punkt[0] - x)
+    dlugosc_y = abs(punkt[1])
+
+    return dlugosc_x + dlugosc_y
 
 def mediana(data):
     if len(data)%2==0:
@@ -10,19 +24,15 @@ def mediana(data):
 def main():
     n = int(input())
     kopce = []
+
     while n:
         x, y = map(int, input().split())
         kopiec = (x, y)
         kopce.append(kopiec)
         n-=1
-
     kopce.sort(key=lambda x: x[0])
 
-    print(kopce)
-    print(mediana(kopce))
-
-
-    return
+    answer(kopce)
 
 if __name__ == '__main__':
     main()
