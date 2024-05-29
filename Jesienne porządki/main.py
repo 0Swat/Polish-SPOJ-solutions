9b52b7f8f0e007078e10a9086cd12ec755428400
Oskar Swat <oskar.swat@tlen.pl>
2024-05-29 15:36:04 +0200

    Mediana, Sorting

--- Jesienne porządki/main.py
+++ Jesienne porządki/main.py
@@ -1,6 +1,27 @@
 import math
 
+
+def mediana(data):
+    if len(data)%2==0:
+        return data[int(len(data)/2-1)][0]
+    else:
+        return data[int(len(data)/2)][0]
+
 def main():
+    n = int(input())
+    kopce = []
+    while n:
+        x, y = map(int, input().split())
+        kopiec = (x, y)
+        kopce.append(kopiec)
+        n-=1
+
+    kopce.sort(key=lambda x: x[0])
+
+    print(kopce)
+    print(mediana(kopce))
+
+
     return
 
 if __name__ == '__main__':
