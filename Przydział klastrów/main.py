52864c72d0caf0869bbf78aa376ca4e199a3cb0e
Oskar Swat <oskar.swat@tlen.pl>
2024-05-30 11:05:21 +0200

    https://pl.spoj.com/submit/WI_KLAST/

--- Przydział klastrów/main.py
+++ Przydział klastrów/main.py
@@ -1,24 +1,77 @@
 class System_WIZUT_2009:
     def __init__(self, n, r):
-        self.cluster = [r for _ in range(n)]
+        self.cluster_size = r
+        self.cluster = r * n
         self.files = []
 
     def PokazClustery(self):
-        print(self.cluster)
+        return int(self.cluster / self.cluster_size)
 
-    def PokazPliki(self):
-        print(self.files)
-
+    def file_exists(self, filename):
+        return any(file[0] == filename for file in self.files)
+    
     def Komenda(self, data_in):
-        print(data_in[0:6])
-        #if data_in[0:6] == "CREATE":
-        #    a, b = map(str, input().split())
-        #if data_in[0:5] == "WRITE":
-        #    a, b, c = map(str, input().split())
-        #if data_in[0:8] == "TRUNCATE":
-        #    a, b, c = map(str, input().split())
-        #if data_in[0:6] == "DELETE":
-        #    a, b = map(str, input().split())
+        command_parts = data_in.split()
+        command = command_parts[0]
+        if command == "CREATE":
+            self.Create(command_parts[1])
+        elif command == "WRITE":
+            self.Write(command_parts[1], int(command_parts[2]))
+        elif command == "TRUNCATE":
+            self.Truncate(command_parts[1], int(command_parts[2]))
+        elif command == "DELETE":
+            self.Delete(command_parts[1])
+
+    def Create(self, name):
+        if self.file_exists(name):
+            #print("Istnieje już taki plik")
+            return
+        else:
+            file = [name, 0]
+            self.files.append(file)
+    
+    def Write(self, name, size):
+        if not self.file_exists(name):
+            #print("Taki plik nie istnieje")
+            return
+        elif size > self.cluster:
+            #print("Brak miejsca")
+            return
+        else:
+            for file in self.files:
+                if file[0] == name:
+                    file[1] += size
+                    self.cluster -= size
+                    break
+
+    def Truncate(self, name, size):
+        if not self.file_exists(name):
+            #print("Taki plik nie istnieje")
+            return
+        else:
+            for file in self.files:
+                if file[0] == name:
+                    if file[1] < size:
+                        #print("Za mały rozmiar pliku")
+                        return
+                    else:
+                        file[1] -= size
+                        self.cluster += size
+                        break
+
+    def Delete(self, name):
+        if not self.file_exists(name):
+            #print("Taki plik nie istnieje")
+            return
+        else:
+           for file in self.files:
+                if file[0] == name:
+                    self.cluster += file[1]
+                    self.files.remove(file)
+
+
+                
+
 
 def main():
     n = int(input())
@@ -29,9 +82,14 @@ def main():
         data = str(input())
         system1.Komenda(data)
         x-=1
-    system1.PokazClustery()
-    system1.PokazPliki()
-    return
+    while True:
+        try:
+            data = str(input())
+            system1.Komenda(data)
+            x-=1
+        except EOFError:
+            break
+    print(system1.PokazClustery())
 
 if __name__ == '__main__':
     main()
