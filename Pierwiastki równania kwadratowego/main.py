936a1d3fc0871518f003a374208b29edacf6646b
Oskar Swat <oskar.swat@tlen.pl>
2024-06-03 14:55:15 +0200

    Giving answer, no k included

--- Pierwiastki równania kwadratowego/main.py
+++ Pierwiastki równania kwadratowego/main.py
@@ -1,5 +1,14 @@
 def answer(a, b, c, k):
-    print(a, b, c, k)
+    delta = b*b - 4*a*c
+    if delta < 0:
+        print('0')
+    elif delta == 0:
+        x = -b / 2*a
+        print('1 ' + str(x))
+    elif delta > 0:
+        x1 = (-1*b - delta**0.5) / 2*a
+        x2 = (-1*b + delta**0.5) / 2*a
+        print('2 ' + str(x1) + ' ' + str(x2))
 
 def main():
     t = int(input())
