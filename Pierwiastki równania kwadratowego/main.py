a8f0f78ed0cca1ee60529d2f30f768823ce80ae4
Oskar Swat <oskar.swat@tlen.pl>
2024-06-03 14:48:44 +0200

    Answer function init

--- Pierwiastki równania kwadratowego/main.py
+++ Pierwiastki równania kwadratowego/main.py
@@ -1,4 +1,12 @@
+def answer(a, b, c, k):
+    print(a, b, c, k)
+
 def main():
+    t = int(input())
+    while t:
+        a, b, c, k = map(int, input().split())
+        answer(a, b, c, k)
+        t-=1
     return
 
 if __name__ == '__main__':
