09aa8209949c1b45077cb6e456c3a9726c843f7b
Oskar Swat <oskar.swat@tlen.pl>
2024-06-05 12:34:45 +0200

    Roman to Decimal working

--- Dodawanie rzymskie/main.py
+++ Dodawanie rzymskie/main.py
@@ -12,13 +12,26 @@ def OneRoman2Decimal(roman):
       elif roman == "D":
            return 500
       elif roman == "M":
-           return 100
+           return 1000
       else:
            return 0
+      
+def Roman2Decimal(roman):
+    return_sum = 0
+
+    for i in range(len(roman)-1):
+        if OneRoman2Decimal(roman[i]) >= OneRoman2Decimal(roman[i+1]):
+            return_sum += OneRoman2Decimal(roman[i])
+        else:
+            return_sum -= OneRoman2Decimal(roman[i])
+    return_sum += OneRoman2Decimal(roman[len(roman)-1])
+
+    return return_sum
     
 
 def main():
     print(OneRoman2Decimal("M"))
+    print(Roman2Decimal("CXXIII"))
     return
 
 if __name__ == '__main__':
