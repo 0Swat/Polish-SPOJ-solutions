45b34a468e0029163d9b52fe44ff1c05b8696977
Oskar Swat <oskar.swat@tlen.pl>
2024-06-05 12:26:25 +0200

    Single number to decimal

--- Dodawanie rzymskie/main.py
+++ Dodawanie rzymskie/main.py
@@ -1,4 +1,24 @@
+def OneRoman2Decimal(roman):
+      if roman == "I":
+           return 1
+      elif roman == "V":
+           return 5
+      elif roman == "X":
+           return 10
+      elif roman == "L":
+           return 50
+      elif roman == "C":
+           return 100
+      elif roman == "D":
+           return 500
+      elif roman == "M":
+           return 100
+      else:
+           return 0
+    
+
 def main():
+    print(OneRoman2Decimal("M"))
     return
 
 if __name__ == '__main__':
