ca915a50d793f69d7e9724596aa2de22b8f4c695
Oskar Swat <oskar.swat@tlen.pl>
2024-06-10 13:53:51 +0200

    Init

---
+++ Łamacz gg/main.py
@@ -0,0 +1,14 @@
+alfabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+def Hack(password):
+    return
+
+def main():
+    while True:
+        try:
+           Hack(str(input())) 
+        except:
+            break
+
+if __name__ == '__main__':
+    main()
