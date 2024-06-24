b4403d602b0fc5e5d3fd1fda80a827d6cec007a6
Oskar Swat <oskar.swat@tlen.pl>
2024-06-24 20:50:21 +0200

    https://pl.spoj.com/problems/KMP/

--- Wyszukiwanie wzorca w tekscie/main.py
+++ Wyszukiwanie wzorca w tekscie/main.py
@@ -1,5 +1,18 @@
+def TemplateFind(size, template, text):
+    text_range = len(text)
+
+    for i in range(text_range - size + 1):
+        if text[i:i+size] == template:
+            print(i)
+
 def main():
-    return
+    t = int(input())
+    while t:
+        size = int(input())
+        template = str(input())
+        text = str(input())
+        TemplateFind(size, template, text)
+        t-=1
 
 if __name__ == '__main__':
     main()
