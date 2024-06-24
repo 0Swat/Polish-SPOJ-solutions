31134bdc33cad258ebd22906c98f76a027466a69
Oskar Swat <oskar.swat@tlen.pl>
2024-06-24 21:19:23 +0200

    Working better version

--- To już jutro/main.py
+++ To już jutro/main.py
@@ -1,14 +1,22 @@
 def Answer(l, c):
-    if c%l == 0:
+    c_len = len(c)
+    temp = ''
+    for i in range(c_len):
+        if i != 0:
+            temp = str(modulo)
+        modulo = int(temp + c[i]) % l
+    
+    if modulo == 0:
         print("TAK")
     else:
         print("NIE")
+
 
 def main():
     t = int(input())
     while t:
         l, c = map(int, input().split())
-        Answer(l, c)
+        Answer(l, str(c))
         t-=1
 
 if __name__ == '__main__':
