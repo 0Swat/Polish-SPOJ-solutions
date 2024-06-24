e9a2e68637447a05b09102040484f1edcea0cf94
Oskar Swat <oskar.swat@tlen.pl>
2024-06-24 21:09:13 +0200

    Easy solution

--- To już jutro/main.py
+++ To już jutro/main.py
@@ -1,7 +1,14 @@
+def Answer(l, c):
+    if c%l == 0:
+        print("TAK")
+    else:
+        print("NIE")
+
 def main():
     t = int(input())
     while t:
-        
+        l, c = map(int, input().split())
+        Answer(l, c)
         t-=1
 
 if __name__ == '__main__':
